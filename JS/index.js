edd40a303dcafe58d05695c1c1820210b14b1141
sagarbista1221 <emigarsagar@gmail.com>
2025-02-09 17:58:08 +0545

    variables and interactions

---
+++ JS/index.js
@@ -0,0 +1,39 @@
+console.log("Hello World"); //print the message
+
+// var,let,const
+
+// ES5 (echma script 5)
+//variable => var
+
+// ES6
+//let,const
+
+const name = "Sagar Bista"; // name variable declare,, override garna dedaina
+console.log(name);
+
+//var name="ramesh" // variable declare
+//name = "ram"; // var ly override garxa
+console.log(name);
+
+let school = "abc"; // let le override garna dinxa
+school = "xyz";
+console.log(school);
+
+////////////////
+
+//alert("Hello World");
+//const user = prompt("What is your business?");
+//console.log(user);
+
+//const isStudent = confirm("are you above 18+?");
+//console.log(isStudent);
+
+// Write a program that asks user for their name and alerts their name
+// with greeting
+
+const username = prompt("What is your name?");
+//1
+const greeter = "Welcome back, " + username;
+alert(greeter);
+//2 String Literal
+alert(`Welcome back, ${username} `);
