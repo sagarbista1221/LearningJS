2a3db0709ff65788c822841cab02ae7cc893db2f
sagarbista1221 <emigarsagar@gmail.com>
2025-02-13 18:07:34 +0545

    functions

---
+++ JS/functions.js
@@ -0,0 +1,61 @@
+// Functions are the building block of application
+
+// principle = DRY donot repeat yourself
+
+// How it works?
+// Function takes Input, uses logic and returns ouput
+
+// Syntax
+
+//ES5
+// function fnName(input) {
+//     logic
+//     return outputs
+// }
+
+// Write a function to add 2 numbers
+// Function declaration / initialization
+// function sum(num1, num2) {
+//   const result = num1 + num2;
+//   return result;
+// }
+// //Input data
+// const value1 = +prompt("Enter the first number");
+// const value2 = +prompt("Enter the Second number");
+// const answer = sum(value1, value2);
+// alert(answer);
+
+// console.log(sum(1,0));
+// console.log(sum(3,2));
+// console.log(sum("Sagar", "Bista"));
+
+// Write a functioin to calculate the area of circle
+
+// function area(radius) {
+//     const result = 3.14 * +radius * +radius;
+//     return result;
+// }
+//function call
+//direct
+// console.log(area("4"));
+// console.log(area(4));
+
+//input value
+// const radius = +prompt("Enter the value of radius");
+// const result = area(radius);
+// alert(result);
+
+/*
+1. Arrow Function (ES6)
+2. default function
+3. Parameterized  function
+4. Closure
+5. IIFEs
+6. Explicit Function
+7. Implicit Function
+8. Anonymous Fuctioin
+9. Inline Function
+10. Callback Function
+*/
+
+//ES6
