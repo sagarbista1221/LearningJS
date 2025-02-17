4a8748146c56440f0c999cbf5e3a6f729f055ec2
sagarbista1221 <emigarsagar@gmail.com>
2025-02-17 18:01:08 +0545

    hw

---
+++ hwpractice/day5.js
@@ -0,0 +1,29 @@
+//Data Types
+//Primitive data type are String, Number, Null, Boolean, Undefined (Default data type is String)
+//Complex data type are Object, date, array
+
+// 1.) String
+const string = "Sagar is learning a programming"; //character, word, sentence, paragraph
+console.log(string);
+
+// 2.)Number
+const pi = 3.14; //Number
+console.log(pi);
+
+// 3.)Bigint
+const largeNumber = BigInt(123456789846545614321);
+console.log(largeNumber);
+
+// 4.)Undefined
+let x; // undefined;
+console.log(x);
+
+// 5.) null
+const d = null; //null
+console.log(d);
+
+// 6.) Boolean
+const isMale = true;
+console.log(isMale);
+
+// Type conversion or Type coercion
